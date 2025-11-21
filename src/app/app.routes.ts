c0853ca8aafc987d77e7a6024ea5b0f7dab30dbe
Angely <angelycorahua2005@gmail.com>
2025-11-20 21:52:46 -0500

    feat: implementar configuración de entorno y rutas para panel de administración

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,16 +8,28 @@ import { CartaComponent } from './features/carta/carta';
 import { AcompComponent } from './features/acompanamientos/acompanamientos';
 import { BebidasComponent } from './features/bebidas/bebidas';
 
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout';
+import { PublicLayoutComponent } from './layouts/public-layout/public-layout';
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'promociones', pathMatch: 'full' }, 
-    { path: 'promociones', component: PromocionesComponent },
-    { path: 'carta', component: CartaComponent },
-    { path: 'acompanamientos', component: AcompComponent },
-    { path: 'bebidas', component: BebidasComponent },
-
-    { path: 'login', component: LoginComponent },
-    { path: 'register', component: RegisterComponent },
-
+    // Rutas públicas CON navbar y footer
+    {
+        path: '',
+        component: PublicLayoutComponent,
+        children: [
+            { path: '', redirectTo: 'promociones', pathMatch: 'full' },
+            { path: 'promociones', component: PromocionesComponent },
+            { path: 'carta', component: CartaComponent },
+            { path: 'acompanamientos', component: AcompComponent },
+            { path: 'bebidas', component: BebidasComponent },
+            { path: 'login', component: LoginComponent },
+            { path: 'register', component: RegisterComponent }
+        ]
+    },
+    
+    // Ruta admin SIN navbar ni footer
+    { path: 'admin', component: AdminLayoutComponent },
+    
+    // Wildcard
     { path: '**', redirectTo: 'promociones' }
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,24 +1,13 @@
-import { Component, signal, inject } from '@angular/core';
-import { RouterOutlet, Router } from '@angular/router';
-import { Navbar } from './shared/navbar/navbar';
-import { Footer } from './shared/footer/footer';
-import { NavbarMenuComponent } from './shared/navbarMenu/navbarMenu';
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Navbar, Footer, NavbarMenuComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
 export class App {
   protected readonly title = signal('frontend');
-
-  private readonly router = inject(Router);
-
-  // hideLayout(): boolean {
-  //   // rutas donde NO se mostrará navbar ni footer
-  //   const hiddenRoutes = ['/login', '/register'];
-  //   return hiddenRoutes.includes(this.router.url);
-  // }
 }
