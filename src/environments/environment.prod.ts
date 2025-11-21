aa3e382e9666b79146e762aae0ee20f78b093669
BruGeth <g15bru03@hotmail.com>
2025-11-20 22:32:37 -0500

    feat: add environment configuration files for production and development

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+// Este archivo es generado automáticamente
+// NO EDITAR MANUALMENTE
+
+export const environment = {
+  production: true,
+  apiUrl: 'https://el-pollo-empoderado-backend-production.up.railway.app/',
+  apiPrefix: '/api',
+  apiVersion: '',
+  appName: 'El Pollo Empoderado',
+  enableDebug: false,
+  tokenKey: 'admin_token',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+// Este archivo es generado automáticamente
+// NO EDITAR MANUALMENTE
+
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:8080',
+  apiPrefix: '/api',
+  apiVersion: '',
+  appName: 'El Pollo Empoderado',
+  enableDebug: true,
+  tokenKey: 'admin_token',
+};
