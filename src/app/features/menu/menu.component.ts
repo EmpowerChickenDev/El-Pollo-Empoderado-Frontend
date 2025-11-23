f61f33bbe293522f9e6b7292d0edf0f78b332c34
Angely <angelycorahua2005@gmail.com>
2025-11-22 20:29:17 -0500

    fix: reorganizar estructura del componente Menu y mejorar estilos

--- src/app/features/menu/menu.component.ts
+++ src/app/features/menu/menu.component.ts
@@ -7,9 +7,29 @@ import { NavbarMenuComponent } from '../../shared/navbarMenu/navbarMenu';
   standalone: true,
   imports: [RouterOutlet, NavbarMenuComponent],
   template: `
-    <app-navbar-menu></app-navbar-menu>
-    <router-outlet></router-outlet>
+    <div class="menu-container">
+      <app-navbar-menu></app-navbar-menu>
+      <div class="menu-content">
+        <router-outlet></router-outlet>
+      </div>
+    </div>
   `,
-  styles: []
+  styles: [`
+    .menu-container {
+      margin-top: -20px;
+    }
+
+    app-navbar-menu {
+      position: fixed;
+      top: 78px;
+      left: 0;
+      right: 0;
+      z-index: 999;
+    }
+
+    .menu-content {
+      margin-top: 60px;
+    }
+  `]
 })
 export class MenuComponent {}
