a1e6d5acde5f2b57f4154337714beda7e9307c1c
Angely <angelycorahua2005@gmail.com>
2025-11-22 20:36:54 -0500

    fix: quitar NavbarMenuComponent no usado del layout público

--- src/app/layouts/public-layout/public-layout.ts
+++ src/app/layouts/public-layout/public-layout.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from '../../shared/navbar/navbar';
-import { NavbarMenuComponent } from '../../shared/navbarMenu/navbarMenu'; // Ajusta la ruta según tu estructura
 import { Footer } from '../../shared/footer/footer';
 
 @Component({
   selector: 'app-public-layout',
   standalone: true,
-  imports: [RouterOutlet, Navbar, NavbarMenuComponent, Footer],
+  imports: [RouterOutlet, Navbar, Footer],
   templateUrl: './public-layout.html',
   styleUrls: ['./public-layout.css']
 })
